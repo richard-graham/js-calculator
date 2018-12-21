3c8aadd216adc263fa646412d036b7488f6f3d5a
Richard Graham <richardcolingraham@gmail.com>
2018-12-21 22:51:26 +1300

    finished

--- logic.js
+++ logic.js
@@ -1,98 +1,7 @@
-/* 
-
-Log button clicks to the console
-
-The button clicks need to be pushed to the calc equation until an operator comes up. if typeof number? (+-%=)
-
-That value needs to be displayed in the cal equation field and should be updated at every event
-
-First calc equation value should be logged to calc output until the 'end enter condition' is met
-
-When an operator is hit the equals function should be initiated
-
-Should we have a global variable that all functions can access?
-
-number / 0 should equals error
-
-Necessary functions:
-Data validation - no double zeros to begin with, if double operators are entered the last on entered should be the one used, only one decimal point per number, an
-operator can't be the first thing entered (i.e a number should be), pressing an operator immediately following = should start a new calculation that operates on the 
-result of the previous evaluation.
-
-Addition function
-Subtraction function
-Multiply function
-Divide function
-Decimal Function
-Numeric function (0-9)
-AC / clear function
-
-keyStroke function {
-    if number push to SECONDVal var
-    if operator add to operator var and call equals function
-    if equals call equals function 
-    if AC call AC function
-}
-
-global vars: 
-firstVal
-operator
-secondVal
-
-function equals () {
-    let varOne = 2;
-    let operator = '+';
-    let varTwo = 5;
-    let output = 0;
-    if (operator === '+') {
-        output = varOne + varTwo;
-    }
-    console.log(output);
-}
-
-equals();
-
-
-
-document.addEventListener('click', buttonIdent());
-
-let equation = [];
-let secondVal = 0;
-let output = secondVal;
-let firstVal = [];
-let operator = [];
-
-
-var buttons = document.getElementsByTagName("button");
-var buttonsCount = buttons.length;
-for (var i = 0; i < buttonsCount; i += 1) {
-    buttons[i].onclick = function(e) {
-    alert(this.id);
-}
-
-
-keyVal = () => {
-    console.log(document.getElementById("seven").value)
-}
-
-keyVal();
-
-//keyStroke = (value) => {
-    
-*/
 document.addEventListener('DOMContentLoaded', function() {
     calcInit();
 }, false);
 
-/*calc = {
-    equation: '',
-    displayValue: '',
-    firstNumber: null,
-    secondNumber: null,
-    operator: null,
-    currentNumber: '',
-    isEqualled: false,
-}  */
 var calc = {};
 
 function calcInit() {
@@ -107,10 +16,7 @@ function calcInit() {
     return calc;
 }
 
-console.log(calc)
-
 function inputNumber(digit) {
-    
     if (calc.isEqualled === true) {
         calcInit()
         console.log(calc)
@@ -132,7 +38,13 @@ function inputNumber(digit) {
 
 function inputOperator (operator) {
     let output = 0;
-
+    if (!isNumber(calc.currentNumber)) {
+        calc.operator = operator;
+        calc.equation = calc.equation.substring(0, calc.equation.length - 3);
+        calc.equation += (` ${operator} `);
+        calc.currentNumber = operator;
+        return;
+    }
     if (calc.operator === null) {
         calc.firstNumber = calc.currentNumber;
         calc.operator = operator;
@@ -146,7 +58,6 @@ function inputOperator (operator) {
         calc.currentNumber = operator;
         calc.equation = calc.firstNumber;
         calc.isEqualled = false;
-        console.log(calc)
     } else {
         calc.secondNumber = calc.currentNumber
         output = doMathStuff(calc.firstNumber, calc.secondNumber, calc.operator)
@@ -155,10 +66,6 @@ function inputOperator (operator) {
         calc.currentNumber = operator;
     }
     calc.equation += (` ${operator} `);
-    
-
-    /*console.log('firstNumber', calc.firstNumber)
-    console.log('secondNumber', calc.secondNumber)*/
 }
 
 function inputEquals(value) {
@@ -168,15 +75,7 @@ function inputEquals(value) {
     calc.displayValue = output;
     calc.equation += (` ${value} ${output.toString()}`)
     document.getElementById('outputDisplay').innerHTML = calc.displayValue;
-    calc.isEqualled = true,
-    console.log('calc', calc);
-
-    console.log('output', output.toString());
-    console.log('value', value);
-    console.log('firstNumber', calc.firstNumber)
-    console.log('secondNumber', calc.secondNumber)
-    console.log('displayValue', calc.displayValue);
-    console.log('equation', calc.equation);
+    calc.isEqualled = true;
 }
 
 function turnStringToNumber(str) {
@@ -234,13 +133,11 @@ document.getElementById('equals').addEventListener('click', function() {
 })
 document.querySelectorAll('button').forEach(function(el){el.addEventListener('click', function() {
     updateEquation();
-    console.log('current number', calc.currentNumber)
 })})
 
 document.getElementById('clear').addEventListener('click', function() {
     calcInit();
-    updateEquation();
-    updateOutputDisplay();
-    console.log(calc)
+    document.getElementById('equation-display').innerHTML = '0'
+    document.getElementById('outputDisplay').innerHTML = '0'
 })
 
