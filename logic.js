6889b2af2781177f736da16dcd749108c62159d9
Richard Graham <richardcolingraham@gmail.com>
2018-12-21 23:11:48 +1300

    trying to revert

--- logic.js
+++ logic.js
@@ -36,7 +36,7 @@ function inputNumber(digit) {
     console.log(calc)
 }
 
-function inputOperator (passedOperator) {
+function inputOperator (operator) {
     let output = 0;
     if (!isNumber(calc.currentNumber)) {
         calc.operator = operator;
@@ -60,11 +60,10 @@ function inputOperator (passedOperator) {
         calc.isEqualled = false;
     } else {
         calc.secondNumber = calc.currentNumber
-        console.log(calc.firstNumber, calc.secondNumber, calc.operator)
         output = doMathStuff(calc.firstNumber, calc.secondNumber, calc.operator)
         calc.firstNumber = output.toString();
-        calc.operator = passedOperator;
-        calc.currentNumber = passedOperator;
+        calc.operator = operator;
+        calc.currentNumber = operator;
     }
     calc.equation += (` ${operator} `);
 }
@@ -73,7 +72,6 @@ function inputEquals(value) {
     var output = 0;
     calc.secondNumber = calc.currentNumber;
     output = doMathStuff(calc.firstNumber, calc.secondNumber, calc.operator)
-    console.log('output =', output);
     calc.displayValue = output;
     calc.equation += (` ${value} ${output.toString()}`)
     document.getElementById('outputDisplay').innerHTML = calc.displayValue;
@@ -88,28 +86,22 @@ function isNumber(string) {
     return parseFloat(string).toString() === string.toString();
 }
 
-function doMathStuff(firstNumberString, secondNumberString, originalOperator) {
+function doMathStuff(firstNumberString, secondNumberString, operator) {
     let returnValue = 0;
     let firstVar = turnStringToNumber(firstNumberString);
     let secondVar = turnStringToNumber(secondNumberString);
-    console.log('firstVar', firstVar)
-    console.log('secondVar', secondVar)
-    console.log('operator', originalOperator)
 
-    if (originalOperator === '+') {
+    if (operator === '+') {
         returnValue = firstVar + secondVar;
-        console.log('pinged')
-    } if (originalOperator === '-') {
+    } else if (operator === '-') {
         returnValue = firstVar - secondVar;
-    } if (originalOperator === '*') {
+    } else if (operator === '*') {
         returnValue = firstVar * secondVar;
-    } if (originalOperator === '/') {
+    } else if (operator === '/') {
         returnValue = firstVar / secondVar;
     }
-    //returnValue = returnValue.toFixed(4);
-    //return parseFloat(returnValue);
-    console.log(returnValue)
-    return returnValue
+    returnValue = returnValue.toFixed(4);
+    return parseFloat(returnValue);
 } 
 
 
@@ -134,7 +126,7 @@ document.querySelectorAll('.number').forEach(function(el){el.addEventListener('c
 })})
 document.querySelectorAll('.operator').forEach(function(el){el.addEventListener('click', function() {
     inputOperator(this.value);
-    //updateOutputOperator()
+    updateOutputOperator()
 })})
 document.getElementById('equals').addEventListener('click', function() {
     inputEquals(this.value);
@@ -148,4 +140,3 @@ document.getElementById('clear').addEventListener('click', function() {
     document.getElementById('equation-display').innerHTML = '0'
     document.getElementById('outputDisplay').innerHTML = '0'
 })
-
